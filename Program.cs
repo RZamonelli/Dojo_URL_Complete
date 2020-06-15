3d9af37664ef7e4f7fc9bdef6be0a95691fcfb96
Renan Zamonelli <47351828+RZamonelli@users.noreply.github.com>
2020-06-15 18:50:39 -0300

    Complete

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace URL
+{
+    class ProcessURL
+    {
+        public void AnalyzeURL(string url)
+        {
+            //Your code here
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ReadKey();
+        }
+
+        private static StreamReader ReadListUrl()
+        {          
+           return null;        
+        }
+    }
+}
